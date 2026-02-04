39f66f1fe4a97ba0beb00bdcd39532c5fbc5b1fa
XFlareT <morgun-kostya@mail.ru>
2026-02-04 18:06:33 +0300

    feat(rtl): add register file

---
+++ rtl/register_file.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.02.2026 17:33:33
+// Design Name: 
+// Module Name: register_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_file(
+     input  logic        clk_i
+    ,input  logic        write_enable_i
+    
+    ,input  logic [ 4:0] write_addr_i
+    ,input  logic [ 4:0] read_addr1_i
+    ,input  logic [ 4:0] read_addr2_i
+    
+    ,input  logic [31:0] write_data_i
+    ,output logic [31:0] read_data1_o
+    ,output logic [31:0] read_data2_o
+);
+
+    logic [31:0] rf_mem [32]; 
+    
+    initial rf_mem[0] = 0;
+    
+    always_ff @(posedge clk_i) begin
+        if(write_enable_i && write_addr_i != 5'b0)
+            rf_mem[write_addr_i] <= write_data_i;
+    end
+
+
+    assign read_data1_o =rf_mem[read_addr1_i];
+    assign read_data2_o =rf_mem[read_addr2_i];
+
+
+endmodule
